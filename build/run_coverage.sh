ad7340b62e601d0157855a498348a535072c2636
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-18 21:43:56 +0000

    Consistently select Debug config, and turn on code coverage

--- build/run_coverage.sh
+++ build/run_coverage.sh
@@ -1,3 +1,3 @@
-cmake .. -DENABLE_CODE_COVERAGE=on
+cmake .. -DENABLE_CODE_COVERAGE=on -DCMAKE_BUILD_TYPE=Debug
 make unit_tests_coverage
 open ./unit_tests_coverage/index.html
--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,3 +1,3 @@
-cmake ..
+cmake .. -DENABLE_CODE_COVERAGE=on -DCMAKE_BUILD_TYPE=Debug
 make unit_tests
 ./unit_tests -r junit > unit_test_result.xml
